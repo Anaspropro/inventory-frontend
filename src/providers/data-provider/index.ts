74d251002f8a9c4b91e6ba16e5b62b8f9d87d616
Webwiz <aliuanas004@gmail.com>
2025-08-08 08:44:10 +0100

    resolving CORs

--- src/providers/data-provider/index.ts
+++ src/providers/data-provider/index.ts
@@ -4,7 +4,7 @@ import dataProviderNestjsxCrud from "@refinedev/nestjsx-crud";
 
 const API_URL =
   process.env.NODE_ENV === "production"
-    ? "https://inventory-backend-xhg7.onrender.com/"
+    ? "https://inventory-backend-xhg7.onrender.com"
     : "http://localhost:3001";
 
 export const dataProvider = dataProviderNestjsxCrud(API_URL); 
